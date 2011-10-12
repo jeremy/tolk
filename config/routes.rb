0f061a0046f7bcf5769fec7bc1036cfdbd26ccd1
Jamis Buck <jamis@37signals.com>
2011-10-12 14:14:40 -0600

    Revert "allow routes to be explicitly mounted to avoid conflicts with existing routes in-app."
    This was supposed to be applied to the rails3 branch, not master.
    
    This reverts commit bb86c405b82ce7d3fa4af386ce0c19783d1d5df3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do
-  Tolk::Routes.mount self
+ActionController::Routing::Routes.draw do |map|
+  map.namespace('tolk') do |tolk|
+    tolk.root :controller => 'locales'
+    tolk.resources :locales, :member => {:all => :get, :updated => :get}
+    tolk.resource :search
+  end
 end
