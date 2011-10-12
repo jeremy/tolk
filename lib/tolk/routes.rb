8f59767d8a9b2de2246cbe9d539d004d5fb3bd52
Jamis Buck <jamis@37signals.com>
2011-10-12 14:14:09 -0600

    allow routes to be explicitly mounted to avoid conflicts with existing routes in-app.
    This is for rails 3.0 compatibility. With rails 3.1 we can make Tolk a
    proper engine with an isolated namespace.

---
+++ lib/tolk/routes.rb
@@ -0,0 +1,26 @@
+module Tolk
+  module Routes
+    @@mounted = false
+    mattr_accessor :mounted
+
+    def self.mount(scope)
+      return if mounted
+      self.mounted = true
+
+      scope.instance_eval do
+        namespace :tolk do
+          root :to => 'locales#index'
+
+          resources :locales do
+            member do
+              get :all
+              get :updated
+            end
+          end
+
+          resource :search
+        end
+      end
+    end
+  end
+end
